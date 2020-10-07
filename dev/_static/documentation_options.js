e6f3409a4f0907383540a4b7da3632e7ad8079bc
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-10-07 16:31:12 +0000

    update dev docs autogenerated from napari/napari@911e095b3554bc89d2ba515f86f78fd978e839c0

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.9.dev31+g647d918',
+    VERSION: '0.3.9.dev32+g911e095',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

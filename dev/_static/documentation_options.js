372bc96ad2d0a6018dc58a244e6f10dd904d6419
Mark Kittisopikul <mkitti@users.noreply.github.com>
2020-10-10 14:51:03 +0000

    update dev docs autogenerated from napari/napari@00a6270c68a2ad7532a1c1c70aa48d2ec595b9e8

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.9.dev38+g07c972b',
+    VERSION: '0.3.9.dev39+g00a6270',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

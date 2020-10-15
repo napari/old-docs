17e3dc05b53c0e242d851c4fd75f768505919922
Matthew Rocklin <mrocklin@gmail.com>
2020-10-15 05:46:40 +0000

    update dev docs autogenerated from napari/napari@e91082522a29b6fc0f3177961d8177bdf45a2089

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.9.dev43+g8e0c920',
+    VERSION: '0.3.9.dev44+ge910825',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

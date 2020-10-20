1c89cab4ead686809aa9ed059ac005fb560cb97c
Abhishek Patil <abhishek@zeroth.me>
2020-10-20 08:01:40 +0000

    update dev docs autogenerated from napari/napari@192facf0b8bc96cb65dffe9f841270b93929fc11

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.9.dev51+g0055113',
+    VERSION: '0.3.9.dev52+g192facf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

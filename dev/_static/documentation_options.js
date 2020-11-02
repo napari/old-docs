e5dbd4c9a23a5fa077282e403e69d9332b8e47b2
Umberto Perco <56282691+UmbWill@users.noreply.github.com>
2020-11-02 05:04:00 +0000

    update dev docs autogenerated from napari/napari@bc3298bb047b00d65133d5e4cf969b62aa65def1

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1.dev14+g205be9f',
+    VERSION: '0.4.1.dev15+gbc3298b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

49534930c28bf93892f9cd4df981c24ab278228e
Abigail McGovern <47407095+AbigailMcGovern@users.noreply.github.com>
2020-11-03 04:43:05 +0000

    update dev docs autogenerated from napari/napari@bfecbe8a0ec4a5ee24762f908c9dc0ea493654f3

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1.dev16+g48387da',
+    VERSION: '0.4.1.dev17+gbfecbe8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

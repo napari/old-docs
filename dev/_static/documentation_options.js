d046b4dda731dfa73a23ac712a8437524561a463
alisterburt <alisterburt@gmail.com>
2020-11-17 20:30:44 +0000

    update dev docs autogenerated from napari/napari@fcbf8cd35274ec46451347b3cbbe455ae999477b

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1a2.dev19+g99e834a',
+    VERSION: '0.4.1a2.dev20+gfcbf8cd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

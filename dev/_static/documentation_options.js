cdcdb938f2da9f5ec6de6ac1adfd55a325c664c1
ziyangczi <41968256+ziyangczi@users.noreply.github.com>
2020-11-23 20:01:23 +0000

    update dev docs autogenerated from napari/napari@65ff2be85b9b04d3c9c20d13a563b40771fb23e9

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1rc3.dev3+g5ecf0c7',
+    VERSION: '0.4.1rc3.dev4+g65ff2be',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

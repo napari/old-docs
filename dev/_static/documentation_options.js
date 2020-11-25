88fb8c43d0637cdb8cfd0ca8af22c1bb0f3393fc
Philip Winston <pwinston@gmail.com>
2020-11-25 16:24:42 +0000

    update dev docs autogenerated from napari/napari@fa9d45edeb97806c501e04a6fe460b82295af8ee

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.2.dev3+g5956643',
+    VERSION: '0.4.2.dev4+gfa9d45e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

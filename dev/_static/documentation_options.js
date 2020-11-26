185d1c517f9301e8b83bfbba4657a3195d44aac0
Alan R Lowe <code@arlowe.co.uk>
2020-11-26 23:44:59 +0000

    update dev docs autogenerated from napari/napari@2c1e09f3b0e283fb5d134e8372bcb8e33f706ff0

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.2.dev8+ge85952c',
+    VERSION: '0.4.2.dev9+g2c1e09f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

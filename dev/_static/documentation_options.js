b68501bb4a938f99b5d18d4c97bea69ad74581cc
Talley Lambert <talley.lambert@gmail.com>
2021-01-11 01:57:59 +0000

    update dev docs autogenerated from napari/napari@ee525896141dd9213cbc9e7ab2af2156bc9c6803

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3.dev69+g7dbebff',
+    VERSION: '0.4.3.dev70+gee52589',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

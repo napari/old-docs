374c70a72a0a6c8fe8e2155952413ad040387a93
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-01-13 03:38:09 +0000

    update dev docs autogenerated from napari/napari@9a9344e0d2648cc9cbce67d404fbfb4595e32f1e

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3rc1.dev1+gf2fc8ed',
+    VERSION: '0.4.3rc1.dev2+g9a9344e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

8f1c58b3976519cc0a91a50400046e4d64090f6f
Christoph Gohlke <cjgohlke@gmail.com>
2021-01-17 17:14:43 +0000

    update dev docs autogenerated from napari/napari@4cecdad60841038b90edc4ff4fc538cf1e7f628f

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev1+g2494842',
+    VERSION: '0.4.4.dev2+g4cecdad',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

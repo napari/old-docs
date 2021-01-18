8dae8dba72b876a5be6ce9c27ce42fb469966c54
dongyaoli10x <65254429+dongyaoli10x@users.noreply.github.com>
2021-01-18 15:52:16 +0000

    update dev docs autogenerated from napari/napari@da4e309e5734931a7ecfc821ae84a33462d80a45

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev2+g4cecdad',
+    VERSION: '0.4.4.dev3+gda4e309',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

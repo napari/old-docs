eab2782e964e811e4b81ae8d0e5caca5daa8b8c0
Volker Hilsenstein <df3iac@amsat.org>
2021-01-24 17:55:41 +0000

    update dev docs autogenerated from napari/napari@1dc317d294097d40ed860db747224f1bfd4dc773

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev17+gbec1c3d',
+    VERSION: '0.4.4.dev18+g1dc317d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

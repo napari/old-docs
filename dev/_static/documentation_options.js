07cd9f7912b39c906b059f469fbb51e764730c92
Grzegorz Bokota <bokota+github@gmail.com>
2021-01-25 17:41:01 +0000

    update dev docs autogenerated from napari/napari@679fd1d7c6d6bd41b9ae0daca642d35af2691c86

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev20+g79fceb2',
+    VERSION: '0.4.4.dev21+g679fd1d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

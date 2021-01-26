d53e4bb6798b4853e2e1546d0a818dcfe628480b
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-01-26 01:20:17 +0000

    update dev docs autogenerated from napari/napari@c1406f67fd5570a70f5d69ae8d956ca5ceee3021

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev22+g5848447',
+    VERSION: '0.4.4.dev23+gc1406f6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

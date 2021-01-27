b85037b4bc5d3ceeaebb960a0496fc4052e6531f
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-01-27 05:16:40 +0000

    update dev docs autogenerated from napari/napari@b8fe7244e6c49b390e8cbfdc04f6d6850b8dda82

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev26+g5e40968',
+    VERSION: '0.4.4.dev27+gb8fe724',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

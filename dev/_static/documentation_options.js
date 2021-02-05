370686986909e42fe11722aba54446a6a82c8322
Jonas Windhager <jwindhager@users.noreply.github.com>
2021-02-05 20:23:22 +0000

    update dev docs autogenerated from napari/napari@b4a492acd19c6ad4d39f199b3b4e84f07b2ac05c

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.5.dev14+gce4a9a1',
+    VERSION: '0.4.5.dev15+gb4a492a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
